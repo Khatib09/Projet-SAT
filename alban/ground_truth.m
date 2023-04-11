b55dde3b634c177a6636e1a4f777e2060e12794d
Bahaddi <Bahaddi@10.1.127.55>
2023-04-11 09:41:03 +0200

    interpolation

--- alban/ground_truth.m
+++ alban/ground_truth.m
@@ -1,18 +1,57 @@
 %% FILE IMPORT
 filename = 'ground_truth.csv';
-T = readtable(filename);
+data = readtable(filename);
 
 %% GROUND TRUTH PLOT
 
-geoplot(T,'LatitudeDegrees','LongitudeDegrees');
+geobasemap satellite
+tlat = data.LatitudeDegrees;
+tlon = data.LongitudeDegrees;
+
+geoplot(tlat,tlon,'-r');
 title 'Ground Truth';
 
 %% SPEED PLOT
 
-%plot(T, 'UnixTimeMillis','SpeedMps');
+speed = data.SpeedMps;
+time = (data.UnixTimeMillis - data.UnixTimeMillis(1)) / 1000;
 
-speed = T.SpeedMps;
-time = (T.UnixTimeMillis - T.UnixTimeMillis(1)) / 1000;
+subplot(2,2,1);
 plot(time,speed);
+xlabel('Temps (s)');
+ylabel('Speed (m/s)');
+title('True Speed');
 
-title 'True Speed';
+%% HEADING PLOT
+
+heading = data.BearingDegrees;
+
+subplot(2,2,2);
+plot(time,heading);
+xlabel('Temps (s)');
+ylabel('Heading (°)');
+title('True Heading');
+
+%% INTERPOLATION SPEED PLOT
+
+
+t = 0:0.1:1849;
+speed_interp = interp1(time, speed, t, 'pchip');
+
+subplot(2,2,3);
+plot(t, speed_interp);
+xlabel('Temps (s)');
+ylabel('Speed (m/s)');
+title('Interpolation Speed')
+
+%% INTERPOLATION HEADING PLOT
+
+
+t = 0:0.1:1849;
+heading_interp = interp1(time, heading, t, 'pchip');
+
+subplot(2,2,4);
+plot(t, heading_interp);
+xlabel('Temps (s)');
+ylabel('Heading (°)');
+title('Interpolation Heading')
